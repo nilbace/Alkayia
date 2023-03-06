ccf9829e48d6812aa207ee80e9bbd059ab031339
nilbace <nilbace@naver.com>
2023-03-06 16:46:16 +0900

    24분정도
    save

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class Moving : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
