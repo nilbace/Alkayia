8ce5adf23c7d0ff84bcfb9028bdb725355745842
nilbace <nilbace@naver.com>
2023-04-01 02:46:33 +0900

    몬스터 정보 받아오기까지
    했다

---
+++ Assets/Scripts/DataBase/DataParsingMachine.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataParsingMachine : MonoBehaviour
+{
+    public TextAsset MonsterDatas;
+    public List<Monster> AllMonList;
+
+    void Start()
+    {
+        string[] line = MonsterDatas.text.Substring(0, MonsterDatas.text.Length - 1).Split('\n');
+        for (int i = 0; i < line.Length; i++)
+        {
+            string[] row = line[i].Split('\t');            
+            AllMonList.Add(new Monster(row[0], int.Parse(row[1]), int.Parse(row[2]), 
+            int.Parse(row[3]), row[4]));
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
+[System.Serializable]
+public class Monster{
+    public string name;
+    public int attackPower;
+    public int attackTerm;
+    public int HP;
+    public string boardExplanation;
+
+    public Monster(string name = "몬스떠!", 
+    int attackPower = 5, 
+    int attackTerm = 5, 
+    int hP = 30
+    , string boardExplanation = "이게 보이면 안된다!")
+    {
+        this.name = name;
+        this.attackPower = attackPower;
+        this.attackTerm = attackTerm;
+        HP = hP;
+        this.boardExplanation = boardExplanation;
+    }
+}
