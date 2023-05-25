4c660c76dffff37518b93668977d6af1c18f022d
nilbace <nilbace@naver.com>
2023-05-25 11:10:23 +0900

    아이템 관리2차

--- Assets/Scripts/UI/Equipment/MyInvenItem.cs
+++ Assets/Scripts/UI/Equipment/MyInvenItem.cs
@@ -9,12 +9,14 @@ public class MyInvenItem : MonoBehaviour
     //아이템 한칸 스크립트
     [SerializeField] Define.Item thisItem;
     public TMPro.TMP_Text _itemName;
+    public TMPro.TMP_Text _itemStat;
     public Image _btnImage;
 
     public void SetItem(Define.Item item)
     {
         thisItem = item;
         _itemName.text = item.ItemName;
+        _itemStat.text = "스텟 : " + item.itemStat;
 
         if(item.image != null)
             _btnImage.sprite = item.image;
@@ -23,7 +25,9 @@ public class MyInvenItem : MonoBehaviour
     {
         UserData.instance.mySaveData.changeItem(thisItem.itemCategory, thisItem.ItemIndex);
 
-        //상점 갱신
+        //인벤토리 닫기
+        print(transform.parent.parent.parent.name);
+        transform.parent.parent.parent.gameObject.SetActive(false);
         MyInventory.instance.ShowMyItems((int)thisItem.itemCategory);
     }
 }
